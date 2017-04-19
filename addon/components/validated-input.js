dc35bb0d56a208a9e72f1ea6b1bc0d96bd6580b6
Vic Amuso <vic@parablesoft.com>
2017-04-19 12:00:25 -0400

    Added isUsingBootstrap support

--- addon/components/validated-input.js
+++ addon/components/validated-input.js
@@ -1,7 +1,27 @@
 import Ember from 'ember';
 import layout from '../templates/components/validated-input';
+import config from "ember-get-config";
 
-export default Ember.Component.extend({
+const {Component,computed} = Ember;
+export default Component.extend({
   layout,
   classNames: ["validated-input-group"],
+  classNameBindings: ["isUsingBootstrap:form-group"],
+  "input-classes": computed("isUsingBootstrap","input-class",function(){
+    let properties = this.getProperties("isUsingBootstrap","input-class");
+    let result = [];
+    result.push(properties["input-class"]);
+    if(properties.isUsingBootstrap){
+      result.push("form-control");
+    }
+    return result.join(" ");
+  }),
+  isUsingBootstrap: computed(function(){
+    if(config["validations-template-helpers"] === undefined || config["validations-template-helpers"]["is-using-bootstrap"] === undefined){
+      return false;
+    }
+    let setting = config["validations-template-helpers"]["is-using-bootstrap"];
+    return setting;
+  }),
+
 });
